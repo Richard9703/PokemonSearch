9c82f41f1083370a57bd006fe3c04ad5ae4f4590
Richard9703 <richardpaulov@gmail.com>
2022-01-05 12:51:49 +0000

    script

--- script.js
+++ script.js
@@ -28,24 +28,48 @@ const fetchImage = () => {
         .catch(err => console.log(err))
 }
 
-const url = 'https://acnhapi.com/v1/fish/1'
+const showFish = arr => {
+    let output = ""
 
-getImage();
-
-async function getImage() {
-    const response = await fetch('https://acnhapi.com/v1/images/fish/1');
-    const blob = await response.blob();
-    document.getElementById('image').src = URL.createObjectURL(blob);
+    arr.forEach(
+        ({name, price, image_uri}) =>
+         (output += `
+         <div class="contents">
+            <div class="content">
+                <div class="content-main">
+                    <img id="image" src=${image_uri}>
+                </div>
+                <div class="content-body">
+                    <span class="fish-name" id="fishname">${name}</span>
+                    <span class="price" id="price"${price}></span>
+                    <button class="content-btn active"><i class="fas fa-heart"></i></button>
+                </div>
+            </div>
+        </div>
+         `)
+            )
+            items.innerHTML = output
 }
+document.addEventListener(fetchImage)
 
-getFish();
+// const url = 'https://acnhapi.com/v1/fish/1'
 
-async function getFish() {
-    const response = await fetch(url);
-    const data = await response.json();
-    const {name, price,} = data;
+// getImage();
+
+// async function getImage() {
+//     const response = await fetch('https://acnhapi.com/v1/images/fish/1');
+//     const blob = await response.blob();
+//     document.getElementById('image').src = URL.createObjectURL(blob);
+// }
+
+// getFish();
+
+// async function getFish() {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     const {name, price,} = data;
         
     
-    document.getElementById('fishname').textContent = name['name-EUen']; //log fish name
-    document.getElementById('price').textContent = price; // log price
-}
+//     document.getElementById('fishname').textContent = name['name-EUen']; //log fish name
+//     document.getElementById('price').textContent = price; // log price
+// }
