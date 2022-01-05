65da82e190da1c514a14e7df0beaaa97968849ea
Richard9703 <richardpaulov@gmail.com>
2022-01-05 12:12:31 +0000

    search js

--- script.js
+++ script.js
@@ -10,7 +10,33 @@
 //     document.getElementById('fish').src = URL.createObjectURL(blob);
 // }
 
+const items = ('https://acnhapi.com/v1a/fish/')
+const search = document.querySelector("#search")
+let fish = []
+
+const fetchImage = () => {
+    fetch(items)
+        .then(res => {
+            res 
+             .json()
+             .then(res => {
+                fish = res
+                showFish(fish)
+             })
+              .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+}
+
 const url = 'https://acnhapi.com/v1/fish/1'
+
+getImage();
+
+async function getImage() {
+    const response = await fetch('https://acnhapi.com/v1/images/fish/1');
+    const blob = await response.blob();
+    document.getElementById('image').src = URL.createObjectURL(blob);
+}
 
 getFish();
 
@@ -18,21 +44,8 @@ async function getFish() {
     const response = await fetch(url);
     const data = await response.json();
     const {name, price,} = data;
+        
     
-
     document.getElementById('fishname').textContent = name['name-EUen']; //log fish name
     document.getElementById('price').textContent = price; // log price
 }
-
-let btn = document.getElementById('btnClick')
-let image = document.getElementById('image')
-
-btn.addEventListener('click', function() {
-    fetch("https://acnhapi.com/v1/images/fish/1")
-    .then(res => res.json())
-    .then(result => {
-        console.log(result)
-        image.src = result.message
-    })
-    .catch(err=>console.log(err))
-})
