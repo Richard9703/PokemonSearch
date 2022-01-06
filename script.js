448d783f5c96322093fa26eeb2729d2667dba83c
Richard9703 <richardpaulov@gmail.com>
2022-01-06 11:26:54 +0000

    search api

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function setup() {
-    createCanvas(200, 200);
-    loadJSON('https://api.genshin.dev/characters', gotData);
+function searchFilm(query) {
+    const url = `https://ghibliapi.herokuapp.com/films?q=${query}`;
+    fetch(url)
+        .then(response => response.json())
+        .then((jsonData) => {
+            const results = jsonData.map(element => element.title);
+            renderResults(results);
+    });
 }
 
-function gotData(data) {
-    println(data);
+function renderResults(results) {
+    const list = document.getElementById("resultsList");
+    list.innerHTML = "";
+    results.forEach(result => {
+        const element = document.createElement("li");
+        element.innerText = result;
+        list.appendChild(element);
+    });
 }
 
-function draw() {
-    background(0);
+window.onload = () => {
+    const searchFieldElement = document.getElementById("searchField");
+    searchFieldElement.onkeyup = (event) => {
+       searchFilm(searchFieldElement.value);
+    };
 }
+
+
